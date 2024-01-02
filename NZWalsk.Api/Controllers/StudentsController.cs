28972a2cf4e83b425b32ddb453663d7149a5625e
LONGMANKE <mburunjoroge0@gmail.com>
2024-01-02 14:27:32 +0300

    Add project files.

---
+++ NZWalsk.Api/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalsk.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "simon", "James", "Bernard", "Brian" };
+            return Ok(studentNames);
+        }
+    }
+}
