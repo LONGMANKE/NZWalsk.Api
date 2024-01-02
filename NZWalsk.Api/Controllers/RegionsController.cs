3dd415cbc030468237eac967d1610387b67bef77
LONGMANKE <mburunjoroge0@gmail.com>
2024-01-02 16:21:55 +0300

    the controller using hard coded data

---
+++ NZWalsk.Api/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalsk.Api.Models.Domain;
+
+namespace NZWalsk.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id= Guid.NewGuid(),
+                    Name= "Nairobi",
+                    Code="NBI",
+                    RegionImageUrl="https://images.unsplash.com/photo-1611144727915-ef30a08aaeb3?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmFpcm9iaSUyMGNpdHl8ZW58MHx8MHx8fDA%3D"
+                },
+                new Region
+                {
+                    Id= Guid.NewGuid(),
+                    Name= "Mombasa",
+                    Code="MBSA",
+                    RegionImageUrl="https://images.unsplash.com/photo-1611144727915-ef30a08aaeb3?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmFpcm9iaSUyMGNpdHl8ZW58MHx8MHx8fDA%3D"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
--- NZWalsk.Api/Data/NZWalksDbContext.cs
+++ NZWalsk.Api/Data/NZWalksDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace NZWalsk.Api.Data
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalsk.Api.Models.Domain;
+
+namespace NZWalsk.Api.Data
 {
-    public class NZWalksDbContext
+    public class NZWalksDbContext:  DbContext
     {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {
+                
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Walk> Walks { get; set; }
     }
 }
