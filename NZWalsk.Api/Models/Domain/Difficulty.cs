f7f95a49b370b29cd18b54b8b6b75218023f1700
LONGMANKE <mburunjoroge0@gmail.com>
2024-01-02 15:00:18 +0300

    fd

--- NZWalsk.Api/Models/Domain/Difficulty.cs
+++ NZWalsk.Api/Models/Domain/Difficulty.cs
@@ -2,5 +2,11 @@
 {
     public class Difficulty
     {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+       
     }
 }
+ 
